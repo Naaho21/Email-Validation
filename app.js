ebc49f8f32f5842403f29d719e17f6caa518ae41
Naaho <Naahohagi11@gmail.com>
2021-02-06 07:59:23 +0300

    style.css

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('Hi There!')
+function validation(){
+    var form = document.getElementById("form");
+    var email = document.getElementById("email").value;
+    var text = document.getElementById("text");
+    var patteron = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+     if(email.match(patteron)){
+         form.classList.add('valid');
+         form.classList.remove('invalid');
+         text.innerHTML  = "Your Email Address Valid"
+         text.style.color = "#00ff00"
+     }else{
+         form.classList.remove('valid');
+         form.classList.add('invalid');
+         text.innerHTML  = "Please Enter Valid Email Address";
+         text.style.color = "#ff0000";
+     }
+
+     if(email == ""){
+        form.classList.remove('valid');
+        form.classList.remove('invalid');
+        text.innerHTML  = ""
+        text.style.color = "#00ff00"
+    }
+
+}
